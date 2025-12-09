ee27a9829789dbb9c987aa737dd00854d0c08b14
KisuraWSP <kisura2003@gmail.com>
2025-12-09 14:16:50 +0530

    modified the repo

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/syntax_survivor/', // <--- THIS MUST MATCH YOUR GITHUB REPO NAME
 })
